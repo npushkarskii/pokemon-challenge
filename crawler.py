bcd879d58d300da25e35bc8a0b0562154b2b93e8
Nikita Pushkarskii <92988368+npushkarskii@users.noreply.github.com>
2021-12-06 16:33:36 -0500

    adds the basic crawler

--- crawler.py
+++ crawler.py
@@ -2,21 +2,59 @@ from coveopush import CoveoPush
 from coveopush import Document
 from coveopush import CoveoPermissions
 from coveopush import CoveoConstants
+import scrapy
 
-sourceId = 'pokemonchallengepebfwi56-tymrhcjeplqwiacezf4rr2cice'
-orgId = 'pokemonchallengepebfwi56'
-apiKey = 'xx0ff4f49e-ddda-45af-a9c0-0e7d899e06db'
 
-push = CoveoPush.Push(sourceId, orgId, apiKey)
-mydoc = Document("https://pokemondb.net/pokedex/national")
-mydoc.Title = "THIS IS A TEST"
-mydoc.SetData("ALL OF THESE WORDS ARE SEARCHABLE")
-mydoc.FileExtension = ".html"
-mydoc.AddMetadata("connectortype", "CSV")
+class PokemonSpider(scrapy.Spider):
+    name = "pokemon_spider"
+    start_urls = ['https://pokemondb.net/pokedex/national']
 
-user_email = "npushkarskii@coveo.com"
-my_permissions = CoveoPermissions.PermissionIdentity(CoveoConstants.Constants.PermissionIdentityType.User, "", user_email)
-allowAnonymous = True
-mydoc.SetAllowedAndDeniedPermissions([my_permissions], [], allowAnonymous)
+    def parse(self, response):
+        pokemon_pages = []
 
-push.AddSingleDocument(mydoc)
+        SET_SELECTOR = '.infocard'
+        for item in response.css(SET_SELECTOR):
+
+            POKEMON_LINK = 'a ::attr(href)'
+            POKEMON_PAGE = 'https://pokemondb.net' + item.css(POKEMON_LINK).extract_first()
+
+            pokemon_pages.append(POKEMON_PAGE)
+
+        for item in pokemon_pages:
+            yield scrapy.Request(item, callback = self.parse_card)
+
+    def parse_card(self, response):
+      
+        NAME_SELECTOR = '#main h1 ::text'
+        IMAGE_SELECTOR = 'a[rel="lightbox"] ::attr(href)'
+        TYPE_SELECTOR = '.vitals-table .type-icon ::text'
+        yield {
+            'name': response.css(NAME_SELECTOR).extract_first(),
+            'image': response.css(IMAGE_SELECTOR).extract_first(),
+            'type': response.css(TYPE_SELECTOR).extract()
+        }
+
+
+
+
+
+
+
+
+# sourceId = 'pokemonchallengepebfwi56-tymrhcjeplqwiacezf4rr2cice'
+# orgId = 'pokemonchallengepebfwi56'
+# apiKey = 'xx0ff4f49e-ddda-45af-a9c0-0e7d899e06db'
+
+# push = CoveoPush.Push(sourceId, orgId, apiKey)
+# mydoc = Document("https://pokemondb.net/pokedex/national")
+# mydoc.Title = "THIS IS A TEST"
+# mydoc.SetData("ALL OF THESE WORDS ARE SEARCHABLE")
+# mydoc.FileExtension = ".html"
+# mydoc.AddMetadata("connectortype", "CSV")
+
+# user_email = "npushkarskii@coveo.com"
+# my_permissions = CoveoPermissions.PermissionIdentity(CoveoConstants.Constants.PermissionIdentityType.User, "", user_email)
+# allowAnonymous = True
+# mydoc.SetAllowedAndDeniedPermissions([my_permissions], [], allowAnonymous)
+
+# push.AddSingleDocument(mydoc)
